d8761856c1413b541b56faa4b97c6b9a030e1282
Adán José-García <adanjoga@gmail.com>
2022-08-17 18:54:12 +0200

    Update august

--- en.search.min.79c994600fc5142329b0ffe1fddd2bf70bb64c930e2f39780aa85473a8b9a553.js
+++ en.search.min.79c994600fc5142329b0ffe1fddd2bf70bb64c930e2f39780aa85473a8b9a553.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.ddfa0775148ad712494cee68438a7628efd0d433ec8bdcbd6d13d7e62e4c9883.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.d30ce27ac74816758a60795dab8777c73c7bfa72bb64012ffb58b1a647f54b3e.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
